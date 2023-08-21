71b00a01db69dfcdfe88e02a759a4b68ccbc3bff
ali64x <htlr780@gmail.com>
2023-08-21 09:46:03 +0300

    edited

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ admin = False
 user = False
 out_of_tries = False
 exit = False
+male_count , female_count =0,0
 filepath="data.txt"
 main_file = open(filepath, "r")
 
@@ -31,4 +32,22 @@ while exit != True :
             print("your salary value is : ",salary_of(UserName,filepath))
         if choice == '2' :
             exit = True
-    
+    if admin == True : # handels admin's interaction 
+        print("1. Display Statistics") 
+        print("2. Add an Employee") 
+        print("3. Display all Employees") 
+        print("4. Change Employee’s Salary") 
+        print("5. Remove Employee") 
+        print("6. Raise Employee’s Salary") 
+        print("7. Exit")
+        choice = input()
+        if choice == '1':
+            for i in main_file:
+                gender = i.strip().split(',')
+                if gender[3] == 'male':
+                    male_count += 1
+                else :
+                    female_count += 1
+            print(f"There are {male_count} male employees , and {female_count} female employees in the company")
+        if choice == '2':
+            
